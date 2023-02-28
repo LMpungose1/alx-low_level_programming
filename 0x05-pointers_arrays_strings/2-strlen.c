2f678e06a3c76c773436d4bf72b7bc59410feed8
LMpungose1 <mpungoselungelo@gmail.com>
2023-02-28 10:31:49 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  _strlen - returns the length of a string
+ * @str: The string to get the length of
+ * Retturn: The length of @str
+ */
+size_t_strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
