41dd2165e205fbf92b353a94879fed5c0e3f6d4c
LMpungose1 <mpungoselungelo@gmail.com>
2023-02-28 10:53:00 -0800

    Tsk 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,9 @@
 /**
  *  _strlen - returns the length of a string
  * @str: The string to get the length of
- * Retturn: The length of @str
+ * Retturn: The length of @str.
  */
-size_t_strlen(const char *str)
+size_t _strlen(const char *str)
 {
 	size_t length = 0;
 
